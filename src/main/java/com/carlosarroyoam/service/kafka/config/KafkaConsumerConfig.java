072b7b336f0692c6eafcb6169571f4a3fbbc1057
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2025-07-26 22:01:56 -0600

    Remueve importaciones sin usar

--- src/main/java/com/carlosarroyoam/service/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/com/carlosarroyoam/service/kafka/config/KafkaConsumerConfig.java
@@ -10,8 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.converter.MessageConverter;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 @Configuration
